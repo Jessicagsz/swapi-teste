ca1c9936ec406b9e48a174d3f2ea33e095c6eb2d
Jessicagsz <84754958+Jessicagsz@users.noreply.github.com>
2025-04-14 15:57:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,47 +18,54 @@ if (isset($_SESSION['results'])) {
   <title>Naves</title>
   <link rel="stylesheet" href="extras/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
+
 </head>
 
 <body>
-  <div class="col-4 mt-4 ms-4 mb-2">
-    <form action="calc.php" id="send" method="POST" onsubmit="handleSubmit()">
-      <div class="first-div">
-        <small class="text-white pb-2">Digite um valor inteiro de MGLT</small>
-        <input type="number" id="" placeholder="Digite um valor" name="mglt_value">
-        <button class="btn btn-sm btn-secondary send-mglt" id="send-mglt" type="submit">Enviar</button>
-      </div>
-    </form>
+  <div class="row justify-content-center h-100 align-content-center mb-2">
+    <div class="col-4">
+      <div class="card">
+        <div class="card-body">
+          <form action="calc.php" id="send" method="POST" onsubmit="handleSubmit()">
+            <div class="first-div d-flex flex-column justify-content-center align-items-center">
+              <h4 class="pb-2 text-center ">Digite um valor inteiro de MGLT</h4>
+              <input type="number" id="" placeholder="Digite um valor" name="mglt_value" class="form-control">
+              <button class="btn btn-primary send-mglt" id="send-mglt" type="submit">Enviar</button>
+            </div>
+          </form>
 
-    <?php
-    if (!empty($results) && is_array($results)) {
-    ?>
-      <table class="table table-hover mt-3">
-        <thead>
-          <tr>
-            <th scope="col"><b>Nave</b></th>
-            <th scope="col"><b>MGLT</b></th>
-          </tr>
-        </thead>
-        <tbody>
+
           <?php
-          foreach ($results as $starship) {
-            $star = explode('%', $starship)[0];
-            $mglt = explode('%', $starship)[1];
+          if (!empty($results) && is_array($results)) {
           ?>
-            <tr>
-              <th scope="row"><?= $star ?></th>
-              <td><?= $mglt ?></td>
-            </tr>
+            <table class="table table-hover mt-3 table-striped table-borderless">
+              <thead>
+                <tr>
+                  <th scope="col"><b>Nave</b></th>
+                  <th scope="col"><b>MGLT</b></th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                foreach ($results as $starship) {
+                  $star = explode('%', $starship)[0];
+                  $mglt = explode('%', $starship)[1];
+                ?>
+                  <tr>
+                    <th scope="row"><?= $star ?></th>
+                    <td><?= $mglt ?></td>
+                  </tr>
+                <?php
+                }
+                ?>
+              </tbody>
+            </table>
           <?php
           }
           ?>
-        </tbody>
-      </table>
-    <?php
-    }
-    ?>
-
+        </div>
+      </div>
+    </div>
   </div>
 </body>
 
